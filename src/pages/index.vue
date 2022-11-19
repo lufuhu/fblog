ad121bfa567618dcd5a89f7852207fda091bcde2
lufuhu <gitea@lufuhu.com>
2022-11-19 22:32:30 +0800

    dev

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+<div>
+  <q-icon name="mat6k_plus" />
+<!--  <q-icon v-for="item in lists" :name="item" />-->
+</div>
+</template>
+
+<script>
+import icons from '@quasar/extras/material-icons/icons.json'
+export default {
+  name: "index",
+  data(){
+    return {
+      lists:[]
+    }
+  },
+  mounted() {
+    console.log(icons)
+    this.lists = icons
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

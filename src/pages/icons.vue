7c89447def6366fbe0f19239c780717abb329c8e
lufuhu <gitea@lufuhu.com>
2022-11-19 23:29:30 +0800

    dev

---
+++ src/pages/icons.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <q-icon v-for="item in lists" :name="item" />
+  </div>
+</template>
+
+<script>
+import icons from '@quasar/extras/material-icons/icons.json';
+import _  from 'lodash/string';
+
+export default {
+  name: "icons",
+  data(){
+    return {
+      lists:[]
+    }
+  },
+  mounted() {
+    let lists = [];
+    for(let i=0;i<icons.length;i++){
+      lists.push(_.snakeCase(icons[i]))
+    }
+    this.lists = lists
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,27 +1,18 @@
 <template>
 <div>
-  <q-icon name="mat6k_plus" />
-  <q-icon v-for="item in lists" :name="item" />
 </div>
 </template>
 
 <script>
-import icons from '@quasar/extras/material-icons/icons.json';
-import _  from 'lodash/string';
 
 export default {
   name: "index",
   data(){
     return {
-      lists:[]
     }
   },
   mounted() {
-    let lists = [];
-    for(let i=0;i<icons.length;i++){
-      lists.push(_.snakeCase(icons[i]))
-    }
-    this.lists = lists
+
   }
 }
 </script>
